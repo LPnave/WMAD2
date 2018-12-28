d8bacb27a0a107e937b1896e32c4e5e8d92e99b5
Pamuditha <pamudthanavaratna@gmail.com>
2018-12-28 23:16:13 +0530

    8th

--- app/src/main/java/pamudithanavaratna/com/styleomega/Activities/Splashscreen.java
+++ app/src/main/java/pamudithanavaratna/com/styleomega/Activities/Splashscreen.java
@@ -4,11 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import pamudithanavaratna.com.styleomega.Database.Login;
+
 public class Splashscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Login testlog = new Login("pamudithanavaratna@gmail.com","123");
+        testlog.save();
+
         startActivity(new Intent(Splashscreen.this,Sign_In.class));
         finish();
     }
